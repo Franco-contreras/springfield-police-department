379fdcb6a2be62c06023f3b113124c02dba0554e
franco contreras <shibateemo@gmail.com>
2022-07-18 18:54:32 -0300

    empty

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-/*
+
 let jefePregunta = prompt('si ha cometido un delito y quiere confesarlo , escriba si , si no es asi escriba no')
 const arrayJefe = ['ha pulsado no, osea que si ha cometido un delito, pero no quiere confesarlo un auto patrulla para su casa', ' estamos enviando una patrulla esta yendo su casa ']
 
 if (jefePregunta === 'si') {
     alert(arrayJefe[1])
 } else alert(arrayJefe[0])
-*/
+
